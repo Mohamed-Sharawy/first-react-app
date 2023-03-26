ee748783ed509b154019fb07c0f96b08dedff9ff
Mohamed Sharawy <96831071+Mohamed-Sharawy@users.noreply.github.com>
2023-03-26 17:31:24 +0200

    Uploud

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import Footer from "../Footer";
+import HeroSec from "../HeroSec";
+function Home() {
+    return (
+        <>
+            <HeroSec />
+            <Cards />
+            <Footer />
+        </>
+    );
+}
+export default Home;
